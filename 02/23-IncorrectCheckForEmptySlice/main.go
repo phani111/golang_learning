3f3ed5e889d06e46a6aad9f1cc5ba4313137f3f7
phaneendra <phani111@gmail.com>
2024-06-19 21:59:12 +0100

    IncorrectCheckForEmptySlic

--- 02/23-IncorrectCheckForEmptySlice/main.go
+++ 02/23-IncorrectCheckForEmptySlice/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	handleOperations("")
 	handleOperationsCorrect("")
@@ -11,4 +13,27 @@ func handleOperations(id string) {
 	if operations != nil {
 		handle(operations)
 	}
+}
+
+func handleOperationsCorrect(id string) {
+	operations := getOperations(id)
+	if len(operations) == 0 { // it is better to check len instead of nil
+		fmt.Println("No operations to handle")
+
+		return
+	}
+	handle(operations)
+}
+
+func handle(operations []float32) {
+	fmt.Println("Do some work with", operations)
+}
+
+func getOperations(id string) []float32 {
+	operations := make([]float32, 0)
+	if id == "" {
+		return operations
+	}
+	operations = append(operations, 1.0)
+	return operations
 }
